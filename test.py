6248ab21cc979c8d85a91c7d2d42a195b87af185
Sera Yang <sera.yang@berkeley.edu>
2018-04-10 01:35:24 -0700

    set up the basic framework for testing, creates a new file and syncs but doesnt test anything else

--- test.py
+++ test.py
@@ -1,12 +1,21 @@
-# run with `python -m unittest`
+# run with `python -m unittest test`
 import unittest
+import os
+import subprocess
+import shutil
 
 class TestStringMethods(unittest.TestCase):
 
+    shutil.rmtree("file")
+    os.mkdir("file")
+    os.chdir("file")
+    p = subprocess.run(["git", "clone", "https://github.com/SeraYang1/OutsideHacks.git"])
+    os.chdir("OutsideHacks")
+    p = subprocess.run(["git", "hub", "sync"])
 
-
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+    def test_sync(self):
+        dir = ".git/git-hub/pull-requests.toml"
+        self.assertTrue(os.path.isdir(dir))
 
     def test_isupper(self):
         self.assertTrue('FOO'.isupper())
@@ -18,6 +27,8 @@ class TestStringMethods(unittest.TestCase):
         # check that s.split fails when the separator is not a string
         with self.assertRaises(TypeError):
             s.split(2)
+
+    os.chdir("../..")
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestStringMethods)
 unittest.TextTestRunner(verbosity=2).run(suite)
